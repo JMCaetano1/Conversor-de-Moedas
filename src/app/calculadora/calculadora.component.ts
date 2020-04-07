2f5d4fb08ae22f525b8b4f3e12825d4e9343bfb4
Marcilio <marcilio@Marcilios-iMac.local>
2020-04-07 11:32:10 -0300

    commit
    Co-authored-by: Lucas Alexandre <lucas.adsjr@gmail.com>

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValueTransformer } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-calculadora',
@@ -8,13 +9,44 @@ import { Component, OnInit } from '@angular/core';
 //teste
 export class CalculadoraComponent implements OnInit {
 
+val:number = 0;
+result:number = 10;
+calculo:number;
+
+saveValue(event) {
+     this.val = Number(event.target.value);
+}
+
+btnMultiplicacao(event){
+   this.calculo = this.val * this.result;
+}
+
+btnDivisao(event){
+  this.calculo = this.val / this.result;
+}
+
+btnSubtracao(event){
+  this.calculo = this.val - this.result;
+}
+
+btnSoma(event){
+  this.calculo = this.val + this.result;
+}
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  // function conversao() {
-  //   var dolarAmericano = 5,24;
+  // conversao() {
+  //   var dolarAmericano = 5.24;
+
+  //   var valor = parseFloat(document.getElementById("text2").value);
+
+  //   if(document.getElementById("opções").value = "Dolar Americano") {
+  //     document.getElementById("valorFinal").innerHTML = (valor/dolarAmericano).toFixed(2);
+  //   }
   // }
 
 }
